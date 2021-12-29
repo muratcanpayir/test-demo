4849c8337f6507277149eabec2af0458fb16f577
Muratcan Payir <muratcanpayir@gmail.com>
2021-12-29 10:43:02 +0000

    color button test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const [nextColor, setNextColor] = useState("blue");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          setButtonColor(buttonColor === "red" ? "blue" : "red");
+          setNextColor(nextColor === "blue" ? "red" : "blue");
+        }}
+        style={{ backgroundColor: buttonColor }}
+      >
+        Change to {nextColor}
+      </button>
+      <input type="checkbox" />
     </div>
   );
 }
